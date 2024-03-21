9cbff437bd543f29d6c9a1c2c9ce29f0f118bf3a
Shyam Venkatraman <132802977+shyam1444@users.noreply.github.com>
2024-03-21 23:49:56 +0530

    Update PAM_code_dmt.R

--- PAM_code_dmt.R
+++ PAM_code_dmt.R
@@ -2,18 +2,23 @@ library(cluster)
 library(tidyverse)
 library(factoextra)
 library(FactoMineR)
+
 df <- read.csv("C:/Users/ShyamVenkatraman/Pictures/data.csv")
-df_numeric <- df[, sapply(df, is.numeric)]
+
 if (nrow(df_numeric) > 0 && ncol(df_numeric) > 0) {
   fviz_nbclust(df_numeric, FUNcluster = pam, method = "silhouette") + theme_classic()
 } else {
   print("No numeric columns found in the dataframe for clustering analysis.")
 }
+fviz_nbclust(df_numeric, FUNcluster = pam, method = "wss") + theme_classic()
+
+#This method shows that the least no of clusters which can be is 2
+fviz_nbclust(df_numeric, FUNcluster = pam, method = "wss") + theme_classic()
 
 myc = pam(df,2,metric = "euclidean", stand = FALSE)
 myc
 
 df_numeric <- df[, sapply(df, is.numeric)]
 df_scaled <- scale(df_numeric)
-my_cluster <- kmeans(df_scaled, centers = 3)
+my_cluster <- kmeans(df_scaled, centers = 2)
 fviz_cluster(my_cluster, data = df_scaled, geom = "point", ellipse.type = "convex", ggtheme = theme_bw())
